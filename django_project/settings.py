75772596c9679ac33ac3871d767e4913526db0c1
Александр Агакин <agakinalexandr@gmail.com>
2023-03-19 19:51:59 +0100

    posts model - views

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,12 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    #3rd party
+    'rest_framework',
     #local
     'accounts',
     'posts'
 ]
-
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
