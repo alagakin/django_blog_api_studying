ac816438c93a4c9f7de9f535b4e0ed4e6ada0359
Александр Агакин <agakinalexandr@gmail.com>
2023-03-20 09:15:43 +0100

    permissions for user view set

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 from .permissions import IsAuthorOrReadOnly
-
 
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
@@ -10,9 +10,10 @@ from .serializers import PostSerializer, UserSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly, )
+    permission_classes = (IsAuthorOrReadOnly,)
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
