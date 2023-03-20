0a49bed933b553819df68bc329432083c66274dd
Александр Агакин <agakinalexandr@gmail.com>
2023-03-20 09:00:40 +0100

    users list and detail endpoints with views

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 from .permissions import IsAuthorOrReadOnly
 
 
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 
 class PostList(generics.ListCreateAPIView):
@@ -16,3 +17,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthorOrReadOnly, )
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
